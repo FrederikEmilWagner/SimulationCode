e02019b2a20eb1d8a993d734446ea94f74ea0857
Frederik Wagner <Frederik.E.Wagner@hotmail.com>
2023-03-10 12:24:20 +0100

    Updated code

---
+++ FEM.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy.sparse
+import scipy.sparse.linalg
+
+import matplotlib.pyplot as plt
+
+
+
+def hej(h):
+    print(h)
--- input/input_files.py
+++ input/input_files.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+# Import libraries
+import numpy as np
+import scipy.sparse
+import scipy.sparse.linalg
+import os
+
+import matplotlib.pyplot as plt
+
+from ypstruct import *
+
+# Set-up for finite element analysis
+study = struct()
+study.meshfile = 'BlaBlaBla'    # Name of mesh-file
+study.analysis = 'static'       # Analysis type: (static)
+
+# Print content in the struct study
+print('\nInformation\n--------------------------------------------------------------')
+print('Mesh-file: '+study.meshfile)
+print('Analysis type: '+study.analysis)
+print('')
+
+
+os.chdir('..')
+print(os.chdir)
+import FEM
